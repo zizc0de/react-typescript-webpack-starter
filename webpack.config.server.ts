3096b6255761520506a0196a97cf61460a3d71c4
Muhammad Abdul Aziz <abdul.aziz@usetada.com>
2021-11-22 18:53:21 +0700

    chore: fix eslint

--- webpack.config.server.ts
+++ webpack.config.server.ts
@@ -7,25 +7,25 @@ import { CleanWebpackPlugin } from 'clean-webpack-plugin';
 
 dotenv.config();
 
-const isDevelopment: boolean = (process.env.ENV === 'development');
+const isDevelopment: boolean = process.env.ENV === 'development';
 
 const config = {
   name: 'server',
   mode: isDevelopment ? 'development' : 'production',
   entry: {
-    server: path.resolve(__dirname, 'src/server/index.ts')
+    server: path.resolve(__dirname, 'src/server/index.ts'),
   },
   output: {
     path: path.resolve(__dirname, 'dist/server'),
-    filename: '[name].js'
+    filename: '[name].js',
   },
   resolve: {
-    extensions: ['.ts', '.tsx']
+    extensions: ['.ts', '.tsx'],
   },
   externals: [nodeExternals()],
   target: 'node',
   node: {
-    __dirname: false
+    __dirname: false,
   },
   module: {
     rules: [
@@ -34,11 +34,11 @@ const config = {
         loader: 'ts-loader',
         options: {
           configFile: 'tsconfig.server.json',
-        }
-      }
-    ]
+        },
+      },
+    ],
   },
-  plugins: [new CleanWebpackPlugin()]
+  plugins: [new CleanWebpackPlugin()],
 };
 
 export type { ServerConfiguration };
